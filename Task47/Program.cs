5eb5a6dbea2d130e4edb20008085725be48c2bc6
Ruslan Kurmaz <rkurmaz@ya.ru>
2023-03-21 20:52:36 +0500

    add Task47 HomeWork

---
+++ Task47/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задайте двумерный массив размером m×n, 
+//заполненный случайными вещественными числами.
+
+double[,] CreateMatrixRndDouble(int rows, int columns, int min, int max)
+{
+    double[,] matrix = new double[rows, columns];
+    Random rnd = new Random();
+    
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            double number = rnd.NextDouble() * (max - min) + min;
+            matrix [i, j] = Math.Round(number, 1);     
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 5} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] RealNumArray2D = CreateMatrixRndDouble(3, 4, -10, 10);
+PrintMatrix(RealNumArray2D);
